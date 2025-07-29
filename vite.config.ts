8448479ac62368a9b31455e0cd7d5a747426bf94
harkon666 <bryan.wicaksanaa@gmail.com>
2025-07-29 12:13:06 +0700

    change constant and config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // Ini yang penting!
+    port: 5173, // Port default Vite, bisa diubah
+  },
   plugins: [react()],
   optimizeDeps: {
     exclude: ["lucide-react"],
